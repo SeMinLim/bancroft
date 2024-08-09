e37cd0f456ba377e73939a1249e03170d97d4c9a
SeMinLim <seminl1@ics.uci.edu>
2024-08-09 12:56:05 +0000

    added reference shrinker

--- refbookmaker/refshrinker/main.cpp
+++ refbookmaker/refshrinker/main.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
+#include <algorithm>
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -19,9 +20,8 @@ using namespace std;
 #define RDCREFSIZE 268435456
 
 
-map<uint32_t, vector<uint64_t>> referenceOrg;
+map<uint32_t, vector<uint64_t>> reference;
 multimap<uint32_t, uint32_t> reference32b;
-vector<uint64_t> referenceRdc;
 
 
 uint32_t refSizeOrg = 2836860451;
@@ -30,8 +30,8 @@ uint32_t refSizeRdc = 268435456;
 uint32_t refInstCnt = 0;
 
 
-uint64_t verification_x[ENCKMERBUFSIZE];
-uint64_t verification_y[ENCKMERBUFSIZE];
+uint64_t verification_x[ENCKMERBUFSIZE] = {0, };
+uint64_t verification_y[ENCKMERBUFSIZE] = {0, };
 
 
 void refReader( char *filename ) {
@@ -45,8 +45,8 @@ void refReader( char *filename ) {
 		}
 		
 		// Insert 256-Mers to Map
-		referenceOrg[i] = vector<uint64_t>{encKmer[0], encKmer[1], encKmer[2], encKmer[3], 
-						   encKmer[4], encKmer[5], encKmer[6], encKmer[7]};
+		reference[i] = vector<uint64_t>{encKmer[0], encKmer[1], encKmer[2], encKmer[3], 
+						encKmer[4], encKmer[5], encKmer[6], encKmer[7]};
 		
 		// Insert 32-bit Portion of 256-mers to MultiMap
 		uint64_t encKmerTmp1 = encKmer[0] << 32;
@@ -61,11 +61,18 @@ void refReader( char *filename ) {
 	}
 
 	f_reference_original.close();
+	printf( "Reading Original Reference File is Done!\n" );
+	fflush( stdout );
 }
 
 void refShrinker( void ) {
 	for ( uint32_t i = 0; i < refSizeRdc; i ++ ) {
-		auto begin = referenceOrg.begin();
+		if ( refSizeRdc % 1000000 == 0 ) {
+			printf( "Reduced Reference: %u\n", i );
+			fflush( stdout );
+		}
+
+		auto begin = reference.begin();
 
 		// Verification Purpose
 		if ( i == 0 ) {
@@ -80,30 +87,39 @@ void refShrinker( void ) {
 
 		vector<uint32_t> index;
 		for ( auto iter = reference32b.lower_bound(encKmer32Bits); 
-			   iter != reference32b.upper_bound(encKmer32Bits); 
-			   iter ++ ) {
+			   iter != reference32b.upper_bound(encKmer32Bits); ) {
 			index.push_back(iter->second);
+			reference32b.erase(iter);
 		}
 
-		for ( uint32_t j = 0; j < index.size(); j ++ ) {
-			referenceOrg.erase(index[j]);
+		sort(index.begin(), index.end());
+
+		for ( uint32_t j = 1; j < index.size(); j ++ ) {
+			reference.erase(index[j]);
 		}
 		
-		for ( uint32_t j = 0; j < ENCKMERBUFSIZE; j ++ ) {
-			referenceRdc.push_back(begin->second[j]);
-		}
+		begin ++;
 	}
+
+	printf( "Shrinking Reference File is Done!\n" );
+	fflush( stdout );
 }
 
 void refWriter( char *filename ) {
+	auto iter = reference.begin();
+
 	ofstream f_reference_reduced(filename, ios::binary);
 	for ( uint32_t i = 0; i < refSizeRdc;  i ++ ) {
 		for ( uint32_t j = 0; j < ENCKMERBUFSIZE; j ++ ) {
-			f_reference_reduced.write(reinterpret_cast<char *>(&referenceRdc[i*ENCKMERBUFSIZE + j]), BINARYRWUNIT);
+			f_reference_reduced.write(reinterpret_cast<char *>(&iter->second[j]), BINARYRWUNIT);
 		}
+		iter++;
 	}
 
 	f_reference_reduced.close();
+
+	printf( "Writing the Reduced Reference File is Done!\n" );
+	fflush( stdout );
 }
 
 
