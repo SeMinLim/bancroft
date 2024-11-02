043b7588afacd5eb572dc2bca301cb7ec71b13e2
SeMinLim <seminl1@ics.uci.edu>
2024-11-02 17:47:16 +0000

    changed the hierarchy of the repository

--- compressor/256mers/vanila/occur-included/main.cpp
+++ compressor/256mers/vanila/occur-included/main.cpp
@@ -62,7 +62,8 @@ uint64_t seqSizeRmnd = 0;
 // Reference: hg19From1 [4KB Unit]
 //uint64_t refSizeUsd = 1071129683;
 //uint64_t refSizeUsd = 1071144232;
-uint64_t refSizeUsd = 1042075569;
+//uint64_t refSizeUsd = 1042075569;
+uint64_t refSizeUsd = 1068841699;
 // Reference: hg19+hg38
 //uint64_t refSizeOrg = 2836860451;
 //uint64_t refSizeUsd = 268435456;
@@ -239,7 +240,7 @@ void compressor( const uint64_t stride ) {
 
 int main( int argc, char **argv ) {
 	char *filenameS = "/mnt/ephemeral/hg16.fasta";
-	char *filenameR = "/home/jovyan/hg19Reference256MersFrom1256MBVer4.bin";
+	char *filenameR = "/mnt/ephemeral/hg19Reference256MersFrom1256MBVer5.bin";
 
 	// Read sequence file
 	seqReader( filenameS );
